ba490259cd20131c7dcd322928c76dca41683a44
Karim M. <karim-mahmoudi@web.de>
2026-05-21 11:57:00 +0200

    Fit profile card inside header dock

--- launcher/src/components/profile/ProfileHeader.tsx
+++ launcher/src/components/profile/ProfileHeader.tsx
@@ -17,7 +17,7 @@ export function ProfileHeader({
   return (
     <section className="relative overflow-hidden border-4 border-black bg-[#fff9ed] shadow-[9px_9px_0_#1f1c0f]">
       <ProfileBanner profile={profile} theme={theme} />
-      <div className="relative z-10 -mt-20 p-5 lg:p-8">
+      <div className="relative z-10 bg-[#fff9ed] px-5 pb-5 pt-4 lg:px-8 lg:pb-8 lg:pt-6">
         <div className="grid gap-6 border-4 border-black bg-[#fff9ed] p-4 shadow-[6px_6px_0_#1f1c0f] lg:grid-cols-[auto_minmax(0,1fr)_300px] lg:items-center">
           <ProfileAvatar profile={profile} />
           <div className="min-w-0">
